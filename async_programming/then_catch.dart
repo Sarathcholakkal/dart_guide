3c8f718356b8abe1a711387b77d33fe7d52a4d2d
sarath <ssarathcholakkall@gmail>
2025-02-12 23:04:44 +0530

    basic folder added

---
+++ async_programming/then_catch.dart
@@ -0,0 +1,114 @@
+//! level 1 one  sync funtion that not  use future value in it/
+// main() {
+//   print("mom said to boy ,please buy milk from market");
+//   longPress();
+//   print("mom contiues to preapare");
+//   print("cake ready");
+// }
+
+// //! Here future method not excecute before that programme execution ended
+// longPress() {
+//   print("boy goes to buy a milk");
+//   Future.delayed(Duration(seconds: 7), () {
+//     print("child comes home");
+//   });
+// }
+
+//! level 2 a sync funtion which is return a future value , still it can't print exact future in main progamme instead only shows future instance..........................................................................
+// main() {
+//   print("mom said to boy ,please buy milk from market");
+//   Future<String> resultdata = longPress();
+//   print(resultdata);//? print future object there not exact return value, main stil sync function that not changed to Futuer function
+//   print("mom contiues to preapare");
+//   print("cake ready");
+// }
+
+// Future<String> longPress() {
+//   print("boy goes to buy a milk");
+//   Future<String> result = Future.delayed(Duration(seconds: 7), () {//? sync function will not able to use await infront of Future type
+//     return "child comes home";
+//   });
+//   return result;
+// }
+
+// //! level 3 a sync funtion which is return a future value , still it print future exact future value in execution when the result arrives from function after some time it's called..........................................................................
+// main() {
+//   print("mom said to boy ,please buy milk from market");
+//   Future<String> resultdata = longPress();
+//   resultdata.then(
+//     (value) {
+//       print(value);// when value recieved it will run in main thread
+//     },
+//   );
+
+//   print("mom contiues to preapare");
+//   print("cake ready");
+// }
+
+// Future<String> longPress() {
+//   print("boy goes to buy a milk");
+//   Future<String> result = Future.delayed(Duration(seconds: 7), () {
+//     //? sync function will not able to use await infront of Future type
+//     return "child comes home (this value from future)";
+//   });
+//   return result;
+// }
+
+//! level 4: what if the future value from sync function will be an error
+
+// main() {
+//   print("mom said to boy ,please buy milk from market");
+//   Future<String> resultdata = longPress();
+//   resultdata.then(
+//     (value) {
+//       print(value); // when value recieved it will run in main thread
+//     },
+//   ).catchError((newError) {
+//     print(newError);
+//   });
+
+//   print("mom contiues to preapare");
+//   print("cake ready");
+// }
+
+// Future<String> longPress() {
+//   print("boy goes to buy a milk");
+//   Future<String> result = Future.delayed(Duration(seconds: 7), () {
+//     //? sync function will not able to use await infront of Future type
+//     // return "child comes home (this value from future)";
+//     throw "No milk in market";
+//   });
+//   return result;
+// }
+//! level 5: How can sent the future value from sync function return a common value wheather success or error(using when complete)
+
+import 'dart:async';
+
+main() {
+  print("mom said to boy ,please buy milk from market");
+  Future<String> resultdata = longPress();
+  resultdata.then(
+    (value) {
+      print(value); // when value recieved it will run in main thread
+    },
+  ).catchError((newError) {
+    print(newError);
+  }).whenComplete(
+    () {
+      print("it will print both case ,error or sucess as finish message");
+    },
+  );
+
+  print("mom contiues to preapare");
+  print("cake ready");
+}
+
+Future<String> longPress() {
+  print("boy goes to buy a milk error");
+  Future<String> result = Future.delayed(Duration(seconds: 7), () {
+    //? sync function will not able to use await infront of Future type
+    // return "child comes home (this value from future)";
+    throw "No milk in market";
+  });
+  return result;
+}
---
+++ basics/fundamental.dart
@@ -0,0 +1,21 @@
+// - **Variable**: we need to store some Data in the memory of a computer, some others need the same data to recall for that we put some name when it's stored it's called a variable. ==when we name stored data, for recall it is called a variable.==
+// - **Data Type**: we don't have the ==same amount of memory for storing text and numbers==, that kind of scenario datatype is going to handle. (int , map., list, double, string, boolean )
+// - **Null:** All datatypes in Dart languages are accepted as objects and their default value is null.(In Java, the default value int is zero, double 0.0, string is  null, list null, boolean false).
+
+// **Var:** var=variable, we do not know or we do not want to specify the data type of variable, then we use the datatype keyword as var .'====Var' type determined by first value is given with this keyword.(if u  give number to a variable it defined as a number).====
+
+// Dynamic: Dart doesn't know anything about its type, it accepts all the types. it may lead to some errors due to the use wrong type in the wrong place.
+
+void main() {
+  print("hello" + "jasmin"); // string interpolation using +
+  int number1 = 1;
+  print(number1);
+  number1 = 20; // reassing an intilaized value
+  print(number1);
+  //int number1 = 34;// number1 alerady decleard error , redeclearation error
+
+  double number2 = 10.5;
+  print(number2);
+  number2 = 11;
+  print(number2); // double have decimal point.
+}
